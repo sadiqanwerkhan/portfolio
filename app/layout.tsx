3d416ee6e8b04fb02544281d474a6848aa01f328
Muhammad Sadiq Anwer Khan <Muhammad Sadiq Anwer Khan>
2024-11-06 23:25:13 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { Sora } from "next/font/google";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+const font = Sora({
+  weight: ["100","200","300","400","500","600","700","800"], // Adjust weights as needed
+  variable: "--font-sora",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Sadiq's Portfolio",
+  description: "Portfolio built on Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={font.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
